7d4c05919264cf52f48495c9ce5a74366c30ed3a
Wataru <wataruoshima@my.smccd.edu>
2022-01-28 20:54:13 -0800

    added the first move

--- scripts/MG400_work.py
+++ scripts/MG400_work.py
@@ -8,12 +8,16 @@ from MG400_autocalib.msg import Coordinate
 import rospy
 import cv_bridge
 import numpy as np
+from bringup.srv import MovJ, DO
 from std_srvs.srv import Empty
 from std_srvs.srv import EmptyResponse
 
 class MOVE:
     def __init__(self):
         print("start MG400")
+        self.arm_move =rospy.ServiceProxy('/bringup/srv/MovJ',MovJ)
+        self.suction = rospy.ServiceProxy('/bringup/srv/DO', DO)
+        first_move = self.arm_move(200, 0, 0, 0, 0, 0)
         self.sub = rospy.Subscriber("/autocalib/coordinate", Coordinate, self.move_callback)
         self.pub_move = rospy.Publisher("/autocalib/move", Int16, queue_size=10)
 
