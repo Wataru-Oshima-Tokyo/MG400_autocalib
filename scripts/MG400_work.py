09a59a1b12ad9a7c8fb8f4152451082d99dd8081
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-01-28 22:22:40 -0800

    added sevice

--- scripts/MG400_work.py
+++ scripts/MG400_work.py
@@ -8,14 +8,16 @@ from MG400_autocalib.msg import Coordinate
 import rospy
 import cv_bridge
 import numpy as np
-from bringup.srv import MovJ, DO
+from bringup.srv import MovJ, DO, EnableRobot, DisableRobot
 from std_srvs.srv import Empty
 from std_srvs.srv import EmptyResponse
-
+import time
 class MOVE:
 	def __init__(self):
 		print("start MG400")
 		self.arm_move =rospy.ServiceProxy('/bringup/srv/MovJ',MovJ)
+		self.arm_enable = rospy.ServiceProxy('/bringup/srv/EnableRobot',EnableRobot)
+                self.arm_disable = rospy.ServiceProxy('/bringup/srv/DisableRobot',DisableRobot)
 		self.suction = rospy.ServiceProxy('/bringup/srv/DO', DO)
 		self.sub = rospy.Subscriber("/autocalib/coordinate", Coordinate, self.move_callback)
 		self.pub_move = rospy.Publisher("/autocalib/move", Int16, queue_size=10)
@@ -42,19 +44,29 @@ class MOVE:
 	def clbk_start_service(self,req):
 		print("start movement ")
 		self.RUN = 1
-		first_move = self.arm_move(200, 0, 0, 0, 0, 0)
-                self.setDefault()
+                self.arm_enable()
+		first_move = self.arm_move(300, 0, 0, 0, 0, 0)
+		time.sleep(1)
+                self.last_clb_time_ = rospy.get_time()
 		return EmptyResponse()
 
 
 	def clbk_stop_service(self,req):
 		print("stop movement")
 		self.RUN = 0
+                self.arm_disable()
 		return EmptyResponse()
 	
 	def move_loop(self):
-		self.last_clb_time_ = rospy.get_time()
-		print("move_loop")
+                init_pos_x = 200
+                init_pos_y = 0
+                for i in range(10):
+                    pos_x = init_pos_x + 10*i
+                    pos_y = init_pos_y + 10*i
+                    self.arm_move(pos_x, pos_y, 0, 0, 0, 0)
+                    time.sleep(1)
+                self.last_clb_time_ = rospy.get_time()
+
 
 
 if __name__ == "__main__":
