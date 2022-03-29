3385a02552c314bbe1ae0db8abb15949b8f40442
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2022-03-28 18:04:34 -0700

    Update MG400_work_automatic.py

--- scripts/MG400_work_automatic.py
+++ scripts/MG400_work_automatic.py
@@ -107,6 +107,7 @@ class MOVE:
             if self.move_stopper:
 #                 self.linetrace_stop()
                 self.move_stopper =False
+		self.work_start_service(Empty)
                 msgs = [msg.x, msg.y, msg.z]
                 x_a, y_a =0,0
                 for i in range(len(self.x_r_coefficient)):
