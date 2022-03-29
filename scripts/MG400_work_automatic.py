883f922cbab66c6eb45b29774e0ea0baffea4ba3
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2022-03-28 18:23:47 -0700

    Update MG400_work_automatic.py

--- scripts/MG400_work_automatic.py
+++ scripts/MG400_work_automatic.py
@@ -107,6 +107,7 @@ class MOVE:
             if self.move_stopper:
 #                 self.linetrace_stop()
                 self.move_stopper =False
+		print("get the message")
 		self.arm_move(300, 0, 0, 0, 0, 0)
                 msgs = [msg.x, msg.y, msg.z]
                 x_a, y_a =0,0
@@ -119,6 +120,7 @@ class MOVE:
                 # x_a = msg.x*self.xx_coefficient + msg.y*self.xy_coefficient + msg.z*self.xz_coefficient +self.x_intercept
                 # y_a = msg.x*self.yx_coefficient + msg.y*self.yy_coefficient + msg.z*self.yz_coefficient+self.y_intercept
                 self.arm_move(x_a,y_a, 0, 0, 0, 0)
+		print("move to", x_a, y_a)
                 time.sleep(1)
 #                 self.arm_move(x_a,y_a,z_a, 0, 0, 0)
                 time.sleep(10)
