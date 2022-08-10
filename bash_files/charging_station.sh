3f6b264c4a2d6d3dbdf19fb2e5213102c11ee342
Wataru Oshima <woshima@mail.csuchico.edu>
2022-08-10 16:42:57 +0900

    added a bash file

---
+++ bash_files/charging_station.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+gnome-terminal --tab -t "roscore" -- bash -c "source /opt/ros/melodic/setup.bash;
+source $HOME/catkin_ws/devel/setup.bash;
+roscore;"
+
+gnome-terminal --tab -t "camera_node" -- bash -c "source /opt/ros/melodic/setup.bash;
+source $HOME/catkin_ws/devel/setup.bash;
+roslaunch realsense2_camera rs_camera.launch align_depth:=true;"
+
+gnome-terminal --tab -t "camera_node" -- bash -c "source /opt/ros/melodic/setup.bash;
+source $HOME/catkin_ws/devel/setup.bash;
+roslaunch MG400_basic MG400_charging_station.launch;"
