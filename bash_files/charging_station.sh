2fb46fa3d960c727e56f6ed541830b5dfb0285af
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-08-26 10:39:22 +0900

    aaa

--- bash_files/charging_station.sh
+++ bash_files/charging_station.sh
@@ -3,7 +3,7 @@
 gnome-terminal --tab -t "roscore" -- bash -c "source /opt/ros/melodic/setup.bash;
 source $HOME/catkin_ws/devel/setup.bash;
 roscore;"
-
+sleep 10
 gnome-terminal --tab -t "camera_node" -- bash -c "source /opt/ros/melodic/setup.bash;
 source $HOME/catkin_ws/devel/setup.bash;
 roslaunch realsense2_camera rs_camera.launch align_depth:=true;"
