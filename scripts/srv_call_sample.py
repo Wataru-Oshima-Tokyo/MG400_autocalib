93abae5a73e0cac3aa4bf84e83ad466552cff2d3
Wataru Oshima <woshima@pocketradar.com>
2023-01-23 00:15:22 +0000

    added smaple file

--- scripts/srv_call_sample.py
+++ scripts/srv_call_sample.py
@@ -16,10 +16,11 @@ if __name__ == "__main__":
     msg_cmd_goal.to = "TK-MG400-001"
     msg_command_client.send_goal(msg_cmd_goal)
     msg_cmd_client_state = action_server_status[msg_command_client.get_state()]
+    now = rospy.Time.sleep()
     while msg_cmd_client_state != action_server_status[3]:
       rospy.loginfo("waiting for the result")
       msg_cmd_client_state = action_server_status[msg_command_client.get_state()]
-      if msg_cmd_client_state == action_server_status[2] or  msg_cmd_client_state == action_server_status[4]:
+      if msg_cmd_client_state == action_server_status[2] or  msg_cmd_client_state == action_server_status[4] or (now+rospy.Duration(msg_cmd_goal.duration + 5) <rospy.Time.now()):
         rospy.loginfo("CHARGING Failed")
         break
       rate.sleep()
